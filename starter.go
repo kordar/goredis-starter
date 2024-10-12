97b30a57cbd6891e823ee4ebb6b10d8bb4d97765
perpel <572821520@qq.com>
2024-10-12 20:44:43 +0800

    m

--- starter.go
+++ starter.go
@@ -37,19 +37,37 @@ func CloseRedisInstance(db string) {
 }
 
 type RedisModule struct {
+	name string
+	load func(moduleName string, itemId string, item map[string]string)
+}
+
+func NewRedisModule(name string, load func(moduleName string, itemId string, item map[string]string)) *RedisModule {
+	return &RedisModule{name, load}
 }
 
 func (m RedisModule) Name() string {
-	return "goredis_starter"
+	return m.name
+}
+
+func (m RedisModule) _load(id string, cfg map[string]string) {
+	if err := goframeworkgoredis.AddRedisInstanceArgs(id, cfg, _tlsConfig, _dialer, _onConnect); err != nil {
+		logger.Fatalf("[%s] initializing goredis: %v", m.Name(), err)
+		return
+	}
+
+	if m.load != nil {
+		m.load(m.name, id, cfg)
+		logger.Debugf("[%s] triggering custom loader completion", m.Name())
+	}
+
+	logger.Infof("[%s] loading module '%s' successfully", m.Name(), id)
 }
 
 func (m RedisModule) Load(value interface{}) {
 	items := cast.ToStringMap(value)
-	for key, val := range items {
-		section := cast.ToStringMapString(val)
-		if err := goframeworkgoredis.AddRedisInstanceArgs(key, section, _tlsConfig, _dialer, _onConnect); err != nil {
-			logger.Errorf("[goredis-starter] 初始化redis异常，err=%v", err)
-		}
+	for key, item := range items {
+		section := cast.ToStringMapString(item)
+		m._load(key, section)
 	}
 }
 
